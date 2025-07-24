21ed405b37565d1c0b89bf786dee72e79deabef0
YashRawat0947 <147089324+YashRawat0947@users.noreply.github.com>
2025-07-24 21:46:59 +0530

    some-changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import Home from './Pages/Home.jsx'
 import Mission from './Pages/Mission.jsx'
 import Overview from './Pages/Overview.jsx'
-import Career from './Pages/career.jsx'
+import Career from './Pages/Career.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
